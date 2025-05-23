1ac404a39387f6089340480bf41ac16e24c555d1
Jinse2092 <96506961+Jinse2092@users.noreply.github.com>
2025-05-23 22:08:09 +0530

    ok

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -143,7 +143,7 @@ const ContactPage = () => {
                 ))}
                 
                 {/* Placeholder for map if needed in future */}
-                <div className="mt-8 h-[42rem] w-[42rem] rounded-lg flex items-center justify-center mx-auto" style={{ background: 'transparent' }}>
+                <div className="mt-8 h-[40rem] w-[40rem] rounded-lg flex items-center justify-center mx-auto" style={{ background: 'transparent' }}>
                     <img src="/forvoqlogo.png" alt="FORVOQ logo" style={{ width: '95%', height: '95%', objectFit: 'contain', background: 'transparent' }} />
                 </div>
               </motion.div>
